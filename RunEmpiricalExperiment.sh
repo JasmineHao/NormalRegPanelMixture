ba8683c41ad138fe1f9919e73cf5f619adcb6529
Jasmine Hao <haojasmine@gmail.com>
2024-04-23 15:57:58 +0800

    update the bash script for running all empirical test

---
+++ RunEmpiricalExperiment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script 1: ChileanTest_Regressor_KL_Spline.R
+Rscript experiment/EmpiricalTests/ChileanTest_Regressor_KL_Spline.R
+Rscript experiment/EmpiricalTests/Japan_Regressor_KL_Spline.R
+
+
+# Script 2: ChileanTest_Regressor_KL_AR1.R
+Rscript experiment/EmpiricalTests/ChileanTest_Regressor_KL_AR1.R
+Rscript experiment/EmpiricalTests/JapanTest_Regressor_KL_AR1.R
+
+# Script 3: ChileanTest_Regressor_KL_AR1_Spline.R
+Rscript experiment/EmpiricalTests/ChileanTest_Regressor_KL_AR1_Spline.R
+Rscript experiment/EmpiricalTests/JapanTest_Regressor_KL_AR1_Spline.R
+
+
+# Script 4: ChileanTest_Regressor_K.R
+Rscript experiment/EmpiricalTests/ChileanTest_Regressor_K.R
+Rscript experiment/EmpiricalTests/JapanTest_Regressor_K.R
+
+# Script 5: ChileanTest_Regressor_K_AR1.R
+Rscript experiment/EmpiricalTests/ChileanTest_Regressor_K_AR1.R
+Rscript experiment/EmpiricalTests/JapanTest_Regressor_K_AR1.R
+
+# Script 6: ChileanTest_AR1.R
+Rscript experiment/EmpiricalTests/ChileanTest_AR1.R
+Rscript experiment/EmpiricalTests/JapanTest_AR1.R
